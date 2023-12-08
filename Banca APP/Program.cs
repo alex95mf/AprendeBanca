a8e23b69535fddf04b25a06b26ab4273d8735418
Alexander Martinez <alex95mf@gmail.com>
2023-12-08 17:16:50 -0500

    NEW: Registro y Login

--- Banca APP/Program.cs
+++ Banca APP/Program.cs
@@ -10,6 +10,7 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // HOLA PRUEBA
     app.UseHsts();
 }
 
@@ -22,6 +23,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Account}/{action=Login}/{id?}");
 
 app.Run();
