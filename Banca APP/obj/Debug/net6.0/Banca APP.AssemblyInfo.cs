ef1001f61bf7b46f9d16a9d1654ef31125efb41c
Kevin <martinezkevin084@gmail.com>
2023-12-10 16:55:32 -0500

    Clonando proyecto en mi pc

--- Banca APP/obj/Debug/net6.0/Banca APP.AssemblyInfo.cs
+++ Banca APP/obj/Debug/net6.0/Banca APP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Banca APP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a0568f80d7b8ab6fee25c816e832846dd5a3c19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Banca APP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Banca APP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
